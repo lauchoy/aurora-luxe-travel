381d9ef1cb5c1d43deb191f99c09e6779ef1652f
Jimmy Lauchoy <7421436+lauchoy@users.noreply.github.com>
2026-01-28 20:39:45 -0800

    feat: integrate concierge form into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import DestinationsGrid from './components/DestinationsGrid';
 import SignatureExperiences from './components/SignatureExperiences';
 import MembershipTiers from './components/MembershipTiers';
 import TestimonialsCarousel from './components/TestimonialsCarousel';
+import ConciergeForm from './components/ConciergeForm';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <SignatureExperiences />
       <MembershipTiers />
       <TestimonialsCarousel />
+      <ConciergeForm />
     </main>
   );
 }
